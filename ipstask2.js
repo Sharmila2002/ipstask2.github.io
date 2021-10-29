15713513674a6ca98ab6de80ccbf96db492b4bee
Sharmila2002 <91255180+Sharmila2002@users.noreply.github.com>
2021-10-30 01:10:09 +0530

    Add files via upload

--- ipstask2.js
+++ ipstask2.js
@@ -1,26 +1,21 @@
  function validate()
  {
-     var user=document.getElementById("email").Value;
-     var pass=document.getElementById("pass").Value;
+     var username=document.getElementById("user").value;
+     var password=document.getElementById("pass").value;
 
-     if((user !="InfoPillar" && pass!="ips@21") ){
-         alert("Please Enter Correct Username as well Password !")
-
-     }
-     else if(user != "Infopillar")
-     {
-         alert("Please Enter Valid Username!")
-     }
-    else if(pass != "ips@21")
-    {
-        alert("Please Enter Valid Password!")
-    }
-     else if (user=="InfoPillar" && pass=="ips@21")
-     {
-         alert("Login Successfully");
-         return false;
-     }
+     if(username=="infopillar20@gmail.com" && password=="ips@20")
+        {
+         alert("Login Successfully")
+        }
+        else if(username !="infopillar20@gmail.com")
+        {
+            alert("Please Enter Correct Username !")
+        }
+        else if(password != "ips@20")
+        {
+            alert("Please Enter Correct Password !")
+        }
      else{
-         alert("Login Failed Please try Again !");
+         alert("Login Failed Please Enter Valid Username & Password ! !");
      }
  }
