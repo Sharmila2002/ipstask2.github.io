6388b21fd5e27080b27b42cb15a9cf6f4452ac90
Sharmila2002 <91255180+Sharmila2002@users.noreply.github.com>
2021-10-30 01:15:35 +0530

    Update ipstask2.js

--- ipstask2.js
+++ ipstask2.js
@@ -7,14 +7,7 @@
         {
          alert("Login Successfully")
         }
-        else if(username !="infopillar20@gmail.com")
-        {
-            alert("Please Enter Correct Username !")
-        }
-        else if(password != "ips@20")
-        {
-            alert("Please Enter Correct Password !")
-        }
+     
      else{
          alert("Login Failed Please Enter Valid Username & Password ! !");
      }
